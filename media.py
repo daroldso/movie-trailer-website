310555451e4d163a1eeb1923da3f069b391a500d
Darold So <daroldso@gmail.com>
2016-04-06 14:22:33 +0800

    Update doc_string for Movie class

--- media.py
+++ media.py
@@ -1,4 +1,5 @@
 class Movie():
+    """Class for favorite movies"""
     def __init__(self, movie_title, movie_desc, movie_poster_image_url,
                  movie_trailer_youtube_url, movie_year):
         self.title = movie_title
