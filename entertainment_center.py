8387f8e8cf08024fb27038fb2c98199ae9be1eea
Darold So <daroldso@gmail.com>
2016-04-06 13:49:12 +0800

    Fix Movie instantiation

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,6 +6,6 @@ favorite_movies = my_favorite.Favorite_Movie().get_favorite_movies()
 movies = []
 
 for movie in favorite_movies:
-    movies.append(media.Movie(movie[0], movie[1], movie[2], movie[3]))
+    movies.append(media.Movie(movie[0], movie[1], movie[2], movie[3], movie[4]))
 
 fresh_tomatoes.open_movies_page(movies)
